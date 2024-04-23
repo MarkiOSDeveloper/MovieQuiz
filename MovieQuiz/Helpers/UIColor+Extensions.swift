c5d4ff98d1599be6183436258466e52d49347840
Mark <mark.borisov.mark@yandex.ru>
2024-04-23 11:00:00 +0300

    Эхпорт шрифтов и цвета с Figma

--- MovieQuiz/Helpers/UIColor+Extensions.swift
+++ MovieQuiz/Helpers/UIColor+Extensions.swift
@@ -1,3 +1,10 @@
 import UIKit
 
-extension UIColor { }
+extension UIColor {
+    static var yp1Green: UIColor { UIColor(named: "YP Green") ?? UIColor.green }
+     static var yp1Red: UIColor { UIColor(named: "YP Red") ?? UIColor.red }
+     static var yp1Black: UIColor { UIColor(named: "YP Black") ?? UIColor.black}
+     static var yp1Background: UIColor { UIColor(named: "YP Background") ?? UIColor.darkGray }
+     static var yp1Gray: UIColor { UIColor(named: "YP Gray") ?? UIColor.gray }
+     static var yp1White: UIColor { UIColor(named: "YP White") ?? UIColor.white}
+}
